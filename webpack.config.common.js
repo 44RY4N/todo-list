57a2adfd508d91d908cc39fb81d630c06050129b
44RY4N <aaryan23b@iiitg.ac.in>
2025-06-06 19:09:08 +0530

    Update Webpack config for GitHub Pages deployment

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,3 @@
-// webpack.config.common.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -7,11 +6,13 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/todo-list/", //here
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      base: "/todo-list/", // Inject <base href="/repo/">
     }),
   ],
   module: {
@@ -23,6 +24,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "[hash][ext][query]",
+        },
       },
     ],
   },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,43 @@
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.config.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    publicPath: "./",
+    publicPath: "/todo-list/",  //here
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin({ filename: "main.css" })],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "main.css" }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_fnames: true, // Preserve function names 
+          keep_classnames: true,
+        },
+      }),
+    ],
+  },
 });
